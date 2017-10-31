aa7c89cdd74b37ff6388bdbfcd7151ddcd94e7ad
Leslie <leslie.khumalo@icloud.com>
2017-10-31 19:10:03 +0000

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Rootcontainer from './Rootcontainer';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Rootcontainer />, div);
-});
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Rootcontainer from './Rootcontainer';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Rootcontainer />, document.getElementById('root'));
-registerServiceWorker();
